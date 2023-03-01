96eb161a373c8a7521e9c33eea85bea80ad50118
太田志幸 <ootashimiyuki@ootashikounoMac-Studio.local>
2023-03-01 15:31:39 +0900

    topページにblogとskill sectionを追加する

--- frontend/src/components/Top/Skill/SkilLItem/SkillItem.tsx
+++ frontend/src/components/Top/Skill/SkilLItem/SkillItem.tsx
@@ -9,7 +9,7 @@ type Props = {
 export const SkillItem: React.FC<Props> = ({ name, description, imgPath }) => {
   return (
     <div className="bg-gray-900 p-4 md:w-1/2 xl:w-1/3">
-      <div className="border-opacity-75 rounded-lg border border-gray-700 p-6 md:h-60">
+      <div className="rounded-lg border border-gray-700 border-opacity-75 p-6 md:h-60">
         <div className="mb-4 inline-flex h-10 w-10 items-center justify-center rounded-full bg-gray-800 text-indigo-400">
           <img src={imgPath} />
         </div>
