f392e3fc690b26279b3d0c7aa7c07f78a737155b
太田志幸 <ootashimiyuki@ootashikounoMac-Studio.local>
2023-03-10 18:50:41 +0900

    add navigation

--- frontend/src/components/Common/Header/Header.tsx
+++ frontend/src/components/Common/Header/Header.tsx
@@ -4,10 +4,10 @@ type Props = {};
 
 export const Header: React.FC<Props> = ({}) => {
   return (
-    <header className="body-font border-b border-b-gray-700 bg-gray-900 text-gray-400">
+    <header className="border-b border-b-gray-700 bg-gray-900 text-gray-400">
       <div className="container mx-auto flex flex-wrap items-center justify-end p-3 md:flex-row">
         <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
-          <a className="mr-5 hover:text-white" href="/">
+          <a className="mr-5 hover:text-white" href="./">
             HOME
           </a>
           <a className="mr-5 hover:text-white" href="./blog">
